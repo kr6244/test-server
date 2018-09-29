a964445ac41845200256429c488b8542d6bd88cb
kr6244 <kaushikraj6244@gmail.com>
2018-09-29 21:52:00 +0530

    Add files via upload

---
+++ newhelloserver.js
@@ -0,0 +1,48 @@
+const express= require('express');
+const hbs =require('hbs');
+var app =express();
+const fs=require('fs');
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+hbs.registerHelper('getdate',()=>
+{
+  return '12 march';
+});
+hbs.registerHelper('capit',(data)=>
+{
+  return data.toUpperCase();
+});
+
+app.use((req,res,next)=>
+{
+console.log('under cons');
+var datas='visited at time :   ' + new Date().toString() + '  method : ' +req.method + ' url : '+ req.url ;
+fs.appendFile('server.log',datas+'\n',(err)=>
+{
+  if(err)
+  {
+    console.log('not able to update');
+  }
+
+});
+next();
+});
+
+app.use(express.static(__dirname+'/html'));
+app.get('/',(req,res)=>
+{
+  res.render('welcome.hbs',{
+    //date : new Date().getFullYear(),
+    con: 'hi bob'
+
+  });
+});
+app.get('/about',(req,res)=>
+{
+  res.render('about.hbs',{
+    //date : new Date().getFullYear(),
+    con: 'this is about page'
+
+  });
+});
+app.listen(3000);
